b3e5175381f8fe68b64dc7344dd9252197bfd7f7
johnpolacek <johnpolacek@gmail.com>
2018-02-03 11:47:11 -0600

    Deploy Next.js to gh-pages

--- _next/303f787d-dcdd-437d-bfd0-08d0ebd2c4b6/page/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([4],{200:function(e,t,u){e.exports=u(201)},201:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=u(6),n=l(a),r=u(98),o=l(r);t.default=function(){return n.default.createElement("div",null,"Hello World. ",n.default.createElement(o.default,{href:"/about",as:"/styled-starter/about"},n.default.createElement("a",null,"About")))}}},[200]);
-            return { page: comp.default }
-          })
-        
