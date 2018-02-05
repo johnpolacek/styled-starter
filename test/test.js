ee1d2da0410182599527d6446f9f1276e92d4adb
johnpolacek <johnpolacek@gmail.com>
2018-02-05 16:35:36 -0600

    configure test build for travis and sauce labs

--- test/test.js
+++ test/test.js
@@ -3,13 +3,10 @@ var webdriver = require('selenium-webdriver'),
     until = webdriver.until;
 
 var chromeCapabilities = webdriver.Capabilities.chrome();
-//setting chrome options to start the browser fully maximized
 var chromeOptions = {
     'args': ['--headless', '--disable-gpu']
 };
-chromeCapabilities.set('chromeOptions', chromeOptions);
-
-
+// chromeCapabilities.set('chromeOptions', chromeOptions); // uncomment to run headless
 
 var test = require('selenium-webdriver/testing');
 var assert = require('assert');
@@ -19,7 +16,19 @@ var driver;
 test.describe( 'SiteNav' , function(){
   
     test.beforeEach(function(){
-    	driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build();
+        if (process.env.SAUCE_USERNAME != undefined) {
+            driver = new webdriver.Builder()
+                .usingServer('http://'+ process.env.SAUCE_USERNAME+':'+process.env.SAUCE_ACCESS_KEY+'@ondemand.saucelabs.com:80/wd/hub')
+                .withCapabilities({
+                    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+                    build: process.env.TRAVIS_BUILD_NUMBER,
+                    username: process.env.SAUCE_USERNAME,
+                    accessKey: process.env.SAUCE_ACCESS_KEY,
+                    browserName: "chrome"
+                }).build();
+        }  else {
+            driver = new webdriver.Builder().withCapabilities(chromeCapabilities).build();
+        }
         driver.get('http://localhost:3000/');
     });
  
