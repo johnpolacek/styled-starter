14c59f59654bf03e847df5e49e3b9a6a1d660ee0
johnpolacek <johnpolacek@gmail.com>
2018-02-06 06:28:02 -0600

    add SiteHeader component

--- src/Index.js
+++ src/Index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Header, Flex, Box, H1, H2, Button, A, Text, Span } from 'styled-system-html';
+import { Box, Text } from 'styled-system-html';
 import SiteNav from './components/SiteNav'
+import SiteHeader from './components/SiteHeader'
 import Link from 'next/link'
 
 
@@ -13,16 +14,7 @@ class Index extends Component {
 		return (
 			<Box>
 				<SiteNav current="Index" />
-				<Header align="center" p={4} bg="green" color="white">
-					<H1 f={7}>
-						<Span color="green4" f={6} position="relative" px={4} style={{top:'-8px'}}>&lt;</Span>
-						Styled Starter
-						<Span color="green4" f={6} position="relative" px={4} style={{top:'-8px'}}>/&gt;</Span>
-					</H1>
-					<Box mw="800px" mx="auto" pb={3}>
-						<H2 f={3} p={3} color="green2">Starter Kit with React, Next.js, Styled System and Styleguidist to get projects going with a design theme, built-in styleguide and&nbsp;browser&nbsp;testing</H2>
-					</Box>
-				</Header>
+				<SiteHeader />
 				<Box p={5} mw="1200px" mx="auto">
 					<Text>Index content will go here...</Text>
 				</Box>
---
+++ src/components/SiteHeader.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Box, Header, H1, H2, Span, A } from 'styled-system-html';
+
+class SiteHeader extends Component {
+	render() {
+	    return (
+	    	<Header align="center" p={4} bg="green" color="white">
+				<H1 f={7}>
+					<Span color="green4" f={6} position="relative" px={4} style={{top:'-8px'}}>&lt;</Span>
+					Styled Starter
+					<Span color="green4" f={6} position="relative" px={4} style={{top:'-8px'}}>/&gt;</Span>
+				</H1>
+				<Box mw="800px" mx="auto" pb={3}>
+					<H2 f={3} p={3} color="green2">Starter Kit with React, Next.js, and Styled Components with Styled System to get projects going with a design theme, built-in styleguide and&nbsp;browser&nbsp;testing</H2>
+				</Box>
+			</Header>
+	    );
+	}
+}
+
+export default SiteHeader;
