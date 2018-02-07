09219d6c0d6cb1f3d880cf91ae8cc7f8daf4959f
johnpolacek <johnpolacek@gmail.com>
2018-02-07 05:54:28 -0600

    Deploy Next.js to gh-pages

--- out/_next/5006680a-5b9d-40d7-9cd8-466e9cd5e219/page/getting-started.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/getting-started', function() {
-            var comp = module.exports=webpackJsonp([6],{297:function(e,t,u){e.exports=u(298)},298:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(2),a=l(n),r=u(299),f=l(r),d=u(108),o=l(d);t.default=function(){return a.default.createElement(o.default,null,a.default.createElement(f.default,null))}},299:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=u(7),a=l(n),r=u(4),f=l(r),d=u(5),o=l(d),c=u(8),i=l(c),s=u(9),_=l(s),m=u(2),p=l(m),x=u(27),E=u(107),v=l(E),h=u(159),M=l(h),b=function(e){function t(){return(0,f.default)(this,t),(0,i.default)(this,(t.__proto__||(0,a.default)(t)).call(this))}return(0,_.default)(t,e),(0,o.default)(t,[{key:"render",value:function(){return p.default.createElement(x.Box,null,p.default.createElement(v.default,{current:"Getting Started"}),p.default.createElement(M.default,null),p.default.createElement(x.Box,{p:[4,5],mw:"1200px",mx:"auto"},p.default.createElement(x.Text,null,"Getting Started content will go here...")))}}]),t}(m.Component);t.default=b}},[297]);
-            return { page: comp.default }
-          })
-        
