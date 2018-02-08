f070022770ea914cdb8f01d50ac0f322538bc70e
johnpolacek <johnpolacek@gmail.com>
2018-02-07 21:51:00 -0600

    use injectGlobal to inject css into head

--- src/containers/Page.js
+++ src/containers/Page.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider, injectGlobal } from 'styled-components';
 import theme 	from '../_Theme';
 import Head		from './Head'
 
@@ -11,3 +11,17 @@ export default (props) => (
     	</ThemeProvider>
   	</div>
 )
+
+// Global style
+// eslint-disable-next-line
+injectGlobal`
+	html{box-sizing:border-box;} *,*:before,*:after{box-sizing:inherit;} 
+	body{margin:0;font-family:'Nunito',sans-serif;line-height:1.6;} 
+	button,input[type=submit]{cursor:pointer;}
+	p{line-height:1.5;}
+	select{padding:8px;}
+	h1,h2,h3,h4,h5,h6,.h1,.h2,.h3,.h4,.h5,.h6{text-rendering:optimizelegibility;margin:0;}
+	input,select,textarea{padding:4px;border-radius:4px;border:solid 1px #ccc;font-size:16px;font-family:'Nunito',sans-serif;}
+	select{-webkit-appearance:menulist;height:32px;}
+	table{border-collapse:collapse;}
+`
