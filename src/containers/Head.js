dc3167412385c8555c0435a73377af1f4afc08e9
johnpolacek <johnpolacek@gmail.com>
2018-02-08 05:41:27 -0600

    update favicon

--- src/containers/Head.js
+++ src/containers/Head.js
@@ -3,6 +3,8 @@ import Head from 'next/head'
 export default (props) => (
 	<Head>
     	<meta charSet="utf-8" />
+    	<link rel="mask-icon" href={props.prefix + '/static/favicon.svg'} />
+    	<link rel="icon" type="image/x-icon" class="js-site-favicon" href={props.prefix + '/static/favicon.ico'} />
 	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	    <meta name="theme-color" content="#000000" />
 	    <title>Styled Starter | Starter Kit with React, Next.js and Styled Components</title>
