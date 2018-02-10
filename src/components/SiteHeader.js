436648dd77712c1c8eca9433e9213b0beaeb7c2e
johnpolacek <johnpolacek@gmail.com>
2018-02-10 09:52:45 -0600

    update download button

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -12,9 +12,9 @@ class SiteHeader extends Component {
 				</H1>
 				<Box mw="800px" mx="auto" pb={3}>
 					<H2 f={[2,3]} p={[2,3]} mb={3} color="rgba(255,255,255,.9)">Starter Kit with React, Next.js, and Styled Components with Styled System to get projects going with a design theme, built-in styleguide and&nbsp;browser&nbsp;testing</H2>
-					<LinkButton py={3} f={3} fontWeight="bold" bg="white" color="blue" href="https://github.com/johnpolacek/styled-starter-basic/archive/master.zip">
+					<LinkButton py={3} f={[2,3]} fontWeight="bold" bg="white" color="blue" href="https://github.com/johnpolacek/styled-starter-basic/archive/master.zip">
 						<Box borderRadius="4px" bg="blue" color="white" px={2} mr={3} pt="2px" style={{transform:'rotate(-90deg)'}}>&#8676;</Box>
-						<Span pr={2}> Download Starter Project</Span>
+						<Span pr={2}> Download Starter Kit</Span>
 					</LinkButton>
 				</Box>
 			</Header>
