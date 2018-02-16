6601c8fc94c07267374062c954934c66fdc80a25
johnpolacek <johnpolacek@gmail.com>
2018-02-15 18:20:54 -0600

    change class attr to className in head component

--- src/containers/Head.js
+++ src/containers/Head.js
@@ -4,7 +4,7 @@ export default (props) => (
 	<Head>
     	<meta charSet="utf-8" />
     	<link rel="mask-icon" href={props.prefix + '/static/favicon.svg'} />
-    	<link rel="icon" type="image/x-icon" class="js-site-favicon" href={props.prefix + '/static/favicon.ico'} />
+    	<link rel="icon" type="image/x-icon" className="js-site-favicon" href={props.prefix + '/static/favicon.ico'} />
 	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	    <meta name="theme-color" content="#000000" />
 	    <title>Styled Starter | Starter Kit with React, Next.js and Styled Components</title>
