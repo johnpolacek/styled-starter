9089b89be31e045f0cab16ed9864ab0be6b3acf9
johnpolacek <johnpolacek@gmail.com>
2018-02-19 20:45:45 -0600

    update theme page

--- src/pages/design/Theme.js
+++ src/pages/design/Theme.js
@@ -3,20 +3,17 @@ import { ThemeProvider } from 'styled-components';
 import { Box, Text } from 'styled-system-html';
 import Heading from '../../components/Heading';
 import Link from '../../components/Link';
-import { LiveProvider, LiveEditor } from 'react-live'
+import CodeBlock from '../../components/CodeBlock'
 
 export default (props) => (
 	<ThemeProvider theme={props.theme}>
 		<Box p={4} mx={1}>
 			<Box pt={3} px={2} mw="960px">
-				<Heading>Theme Editor</Heading>
-				<Text>Edit the JSON below to change theme settings.</Text>
+				<Heading>Theme JSON</Heading>
 				{
 					props.theme &&
 					<Box my={2} bg="white">
-						<LiveProvider mountStylesheet={false} code={JSON.stringify(props.theme, null, 2)}>
-							<LiveEditor style={{fontFamily: props.theme.monospace, padding: '24px', margin:0, height: '100%', fontSize: '14px', color:'#222'}} />
-						</LiveProvider>
+						<CodeBlock>{JSON.stringify(props.theme, null, 2)}</CodeBlock>
 					</Box>
 				}
 			</Box>
